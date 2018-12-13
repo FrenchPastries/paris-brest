3f6ed2191a43fde595148934a53b684611d5e2df
Guillaume Hivert <guillaume.hivert@outlook.com>
2018-12-13 18:54:45 +0100

    :recycle: Refactor Effect to add map as member function

--- src/effect.js
+++ src/effect.js
@@ -1,15 +1,31 @@
-const success = (msg, effect) => ({
+class Effect {
+  constructor({ msg, err, body }) {
+    this.msg = msg
+    this.err = err
+    this.body = body
+  }
+
+  map(mapper) {
+    return new Effect({
+      msg: this.msg,
+      err: this.err,
+      body: this.body.then(mapper)
+    })
+  }
+}
+
+const success = (msg, effect) => new Effect({
   msg,
   body: effect
 })
 
-const attempt = (successMsg, errorMsg, effect) => ({
+const attempt = (successMsg, errorMsg, effect) => new Effect({
   msg: successMsg,
   err: errorMsg,
   body: effect
 })
 
-const failure = (msg, effect) => ({
+const failure = (msg, effect) => new Effect({
   err: msg,
   body: effect
 })
